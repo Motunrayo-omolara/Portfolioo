ac0ee82ce0bd74aaf7a206666522a16c5ed72047
Motunrayo20 <120575741+Motunrayo20@users.noreply.github.com>
2023-12-20 15:19:07 +0100

    Portfolio

---
+++ src/components/Projects.js
@@ -0,0 +1,97 @@
+import { Col, Container, Tab, Row, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard"
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgURL: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgURL: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgURL: projImg3,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgURL: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgURL: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgURL: projImg1,
+        },
+    ];
+
+    
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                    <TrackVisibility>
+                {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__bounce" : ""}>
+                    <h2>Projects</h2>
+                    <p>My projects</p>
+                </div>}
+                </TrackVisibility>
+                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                        <Nav.Item>
+                            <Nav.Link eventKey="first">Tab One</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="second">Tab two</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="third">
+                                Tab Three
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                    <Tab.Content>
+                        <Tab.Pane eventKey="first">
+                            <Row>
+                                {
+                                    projects.map ((project, index) => {
+                                        return (
+                                            <ProjectCard
+                                            key={index}
+                                            {...project}
+                                            />
+                                        )
+                                    })
+                                }
+                            </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second">Omolara</Tab.Pane>
+                        <Tab.Pane eventKey="third">Omolara</Tab.Pane>
+                    </Tab.Content>
+                    </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2} alt="me"></img>
+        </section>
+    )
+}
